2d0d49823bb77dd91e37df569cec657fed5a4563
nekokaito <siddiqsazzad001@gmail.com>
2024-03-03 14:11:23 +0600

    added innerHTML and fixed some position

--- JS/api.js
+++ JS/api.js
@@ -2,6 +2,7 @@ const loadDiscussAPI = async () => {
         const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
         const data = await res.json();
         const posts = data.posts;
+        displayDiscuss(posts);
 }
 
 const loadPostsAPI = async () => {
--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,57 @@
-console.log('main');
+const displayDiscuss = (posts) => {
+    const discussPost = document.getElementById('d-content');
+   
+      posts.forEach(post => {
+               const card = document.createElement('div');
+            
+               card.innerHTML = `<div id="post-${post.id}" class="flex mx-4 mt-6 bg-[#F3F3F5] p-10 rounded-3xl gap-4 relative">
+               <div class="max-w-20 rounded-xl">
+                   <div class="flex justify-end">
+                       <span
+                       id="status-id" class="dot bg-green-500 h-3 w-3 inline-block rounded-full border-2 border-white"
+                    ></span>
+                   </div>
+                   
+                   <img
+                     class="w-full rounded-xl"
+                     src="${post.image}"
+                     alt=""
+                   />
+                   
+                 </div>
+                 <div class="space-y-4">
+                   <div class="flex flex-col lg:flex-row gap-4">
+                     <h3 class="font-bold text-[#12132D99]">#${post.category}</h3>
+                     <h3 class="font-bold text-[#12132D99]">Author : ${post.author.name}</h3>
+                     
+                   </div>
+                   <h1 class="lg:text-2xl font-extrabold">
+                   ${post.title}
+                   </h1>
+                   <p class="text-[#12132D99]">
+                   ${post.description}
+                   </p>
+                   <hr class="border-gray-300 border-dashed" />
+                   <div class="flex gap-4">
+                     <div>
+                       <h4 class="text-[#12132D99]"><i class="fa-regular fa-message mr-2"></i>${post.comment_count}</h1>
+                     </div>
+                     <div>
+                       <h4 class="text-[#12132D99]"><i class="fa-regular fa-eye mr-2"></i> ${post.view_count}</h1>
+                     </div>
+                     <div>
+                       <h4 class="text-[#12132D99]"><i class="fa-regular fa-clock mr-2"></i>${post.posted_time} min</h1>
+                     </div>
+                   </div>
+                  <div class="flex justify-end absolute bottom-7 right-8">
+                   <button
+                   onclick="" class="btn rounded-full bg-green-300"
+                   > <i class="fa-solid fa-envelope-open text-white"></i>
+                   </button>
+                  </div>
+                 </div>
+           </div>
+               `
+               discussPost.appendChild(card);
+      });
+}
