156d6cd3f9c23bec851b109201bf8f6693714436
nekokaito <siddiqsazzad001@gmail.com>
2024-03-03 13:34:53 +0600

    API Fetch done

--- JS/api.js
+++ JS/api.js
@@ -1,1 +1,14 @@
-console.log('api');
+const loadDiscussAPI = async () => {
+        const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+        const data = await res.json();
+        const posts = data.posts;
+}
+
+const loadPostsAPI = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await res.json();
+    // console.log(data[2].author.name);
+}
+
+loadDiscussAPI();
+loadPostsAPI();
